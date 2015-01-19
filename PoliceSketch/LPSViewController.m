f5c8dc4f9f21cbc1dd2712d11e51aeac77d2d58f
Calvin Nisbet <cnisb044@uottawa.ca>
2015-01-19 13:37:22 -0800

    Almost Finished

--- PoliceSketch/LPSViewController.m
+++ PoliceSketch/LPSViewController.m
@@ -17,12 +17,12 @@
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-
+    
+    
+    #pragma mark - Image Arrays
     self.topImages = [NSArray arrayWithObjects:[UIImage imageNamed:@"eyes_1.jpg"], [UIImage imageNamed:@"eyes_1.jpg"], [UIImage imageNamed:@"eyes_2.jpg"], [UIImage imageNamed:@"eyes_3.jpg"], [UIImage imageNamed:@"eyes_4.jpg"], [UIImage imageNamed:@"eyes_5.jpg"], nil];
 
-    
     self.middleImages = [NSArray arrayWithObjects:[UIImage imageNamed:@"nose_2.jpg"], [UIImage imageNamed:@"nose_3.jpg"], [UIImage imageNamed:@"nose_4.jpg"], [UIImage imageNamed:@"nose_5.jpg"], [UIImage imageNamed:@"nose_1.jpg"], nil];
-    
     
     self.bottomImages = [NSArray arrayWithObjects:[UIImage imageNamed:@"mouth_1.jpg"], [UIImage imageNamed:@"mouth_2.jpg"], [UIImage imageNamed:@"mouth_3.jpg"], [UIImage imageNamed:@"mouth_4.jpg"], [UIImage imageNamed:@"mouth_5.jpg"], nil];
 
@@ -30,52 +30,49 @@
     
 }
 
-
+    #pragma mark - IBActions
 - (IBAction)topForward:(id)sender {
     
-      NSArray *topImages = [NSArray arrayWithObjects:[UIImage imageNamed:@"/Users/calvinnisbet/Desktop/PoliceSketchLL/Images/eyes_1.jpg"], [UIImage imageNamed:@"/Users/calvinnisbet/Desktop/PoliceSketchLL/Images/eyes_2.jpg"], [UIImage imageNamed:@"eyes_2.jpg"], [UIImage imageNamed:@"eyes_3.jpg"], [UIImage imageNamed:@"eyes_4.jpg"], [UIImage imageNamed:@"eyes_5.jpg"], nil];
-    UIImageView *topPicture = [[UIImageView alloc] init];
-    self.topPicture = [topImages objectAtIndex:0];
-    [self.view addSubview:topPicture];
-        for (int i = 0; i < [topImages count]; i++) {
-        UIImageView *topPicture = [topImages objectAtIndex:i];
-            [self.view addSubview:topPicture];
-//        topPicture.image = [[UIImageView alloc] initWithImage:[topImages objectAtIndex:0]];
+    if (_currentImage +1 < [_topImages count]) {
+        _currentImage++;
+        UIImage *image = [_topImages objectAtIndex:_currentImage];
+        [_topPicture setImage:image];
     }
-   
+    NSLog(@"Photo Index:%ld", (long)_currentImage);
 }
+
 
 
 - (IBAction)topBack:(id)sender {
     
-    NSArray *topImages = [NSArray arrayWithObjects:[UIImage imageNamed:@"eyes_1.jpg"], [UIImage imageNamed:@"eyes_1.jpg"], [UIImage imageNamed:@"eyes_2.jpg"], [UIImage imageNamed:@"eyes_3.jpg"], [UIImage imageNamed:@"eyes_4.jpg"], [UIImage imageNamed:@"eyes_5.jpg"], nil];
     
-    for (int i = 0; i < [topImages count]; i--) {
-        UIImageView *topPicture = [topImages objectAtIndex:i];
-        self.topPicture = [[UIImageView alloc] initWithImage:[topImages objectAtIndex:0]];
-        [self.view addSubview:topPicture];
+    //Can't just take the same approach as above - need to 
+    
+    if (_currentImage +1 < [_topImages count]) {
+        _currentImage++;
+        UIImage *image = [_topImages objectAtIndex:_currentImage];
+        [_topPicture setImage:image];
     }
-    
-    
+    NSLog(@"Photo Index:%ld", (long)_currentImage);
 }
 
 - (IBAction)middleForward:(id)sender {
     
-    NSArray *middleImages = [NSArray arrayWithObjects:[UIImage imageNamed:@"nose_2.jpg"], [UIImage imageNamed:@"nose_3.jpg"], [UIImage imageNamed:@"nose_4.jpg"], [UIImage imageNamed:@"nose_5.jpg"], [UIImage imageNamed:@"nose_1.jpg"], nil];
-    for (int i = 0; i < [middleImages count]; i++) {
-        UIImageView *middlePicture = [middleImages objectAtIndex:i];
-        self.middlePicture = [[UIImageView alloc] initWithImage:[middleImages objectAtIndex:0]];
-        [self.view addSubview:middlePicture];
+    if (_currentImage +1 < [_middleImages count]) {
+        _currentImage++;
+        UIImage *image = [_middleImages objectAtIndex:_currentImage];
+        [_middlePicture setImage:image];
     }
+    NSLog(@"Photo Index:%ld", (long)_currentImage);
 }
 
 
 - (IBAction)middleBack:(id)sender {
-    NSArray *middleImages = [NSArray arrayWithObjects:[UIImage imageNamed:@"nose_2.jpg"], [UIImage imageNamed:@"nose_3.jpg"], [UIImage imageNamed:@"nose_4.jpg"], [UIImage imageNamed:@"nose_5.jpg"], [UIImage imageNamed:@"nose_1.jpg"], nil];
-    
-    for (int i = 0; i < [middleImages count]; i--) {
-        UIImageView *middlePicture2 = [middleImages objectAtIndex:i];
-//        middlePicture2.image = self.middlePicture.middleImages;
+  
+    if (_currentImage +1 < [_middleImages count]) {
+        _currentImage++;
+        UIImage *image = [_middleImages objectAtIndex:_currentImage];
+        [_middlePicture setImage:image];
     }
 }
 
@@ -83,6 +80,11 @@
 - (IBAction)bottomForward:(id)sender
 {
 
+    if (_currentImage +1 < [_bottomImages count]) {
+        _currentImage++;
+        UIImage *image = [_bottomImages objectAtIndex:_currentImage];
+        [_bottomPicture setImage:image];
+    }
  
 }
 
@@ -91,19 +93,10 @@
 {
 
     if (_currentImage +1 < [_bottomImages count]) {
-        _currentImage++;
+        _currentImage--;
         UIImage *image = [_bottomImages objectAtIndex:_currentImage];
         [_bottomPicture setImage:image];
     }
-    
-    
-    
-//    NSArray *bottomImages = [NSArray arrayWithObjects:[UIImage imageNamed:@"mouth_1.jpg"], [UIImage imageNamed:@"mouth_2.jpg"], [UIImage imageNamed:@"mouth_3.jpg"], [UIImage imageNamed:@"mouth_4.jpg"], [UIImage imageNamed:@"mouth_5.jpg"], nil];
-//   
-//    for (int i = 0; i < [bottomImages count]; i--) {
-//        UIImageView *bottomPicture2 = [bottomImages objectAtIndex:i];
-//        bottomPicture2.image = [bottomImages objectAtIndex:0];
-//    }
 }
 
 
